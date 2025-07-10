962584b1344c63a5f058199e3a6e7297856e0bff
Bada <1127bada@gmail.com>
2025-07-10 11:01:00 +0900

    알고리즘 문제풀이

---
+++ Programmers/python/pg_1845.py
@@ -0,0 +1,7 @@
+# 10:48 ~ 
+
+def solution(nums):
+    return len(nums)//2 if len(set(nums)) >= len(nums)//2 else len(set(nums))
+    # return min(len(nums)//2, len(set(nums))) 도 가능함
+
+print(solution([3,1,2,3]))
